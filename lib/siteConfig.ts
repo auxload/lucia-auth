1b74a20f5af2c55a664f2db9e5cff4e865a3e7f9
auxload <gd69435@gmail.com>
2024-05-09 08:32:39 +0300

    gdfhf

--- lib/siteConfig.ts
+++ lib/siteConfig.ts
@@ -4,5 +4,16 @@ export const navLinks = [
     path: "/dashboard",
     secure: true,
   },
-
+  {
+    label: "Home",
+    path: "/",
+  },
+  {
+    label: "Features",
+    path: "/fee",
+  },
+  {
+    label: "Documentation",
+    path: "/docs",
+  },
 ];
